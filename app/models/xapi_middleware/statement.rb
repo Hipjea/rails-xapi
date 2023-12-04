1a8f90664aa15025464f75f4bfd7fef431c668dd
Hipjea <pierre.duverneix@gmail.com>
2023-12-04 16:17:30 +0100

    statement json

--- app/models/xapi_middleware/statement.rb
+++ app/models/xapi_middleware/statement.rb
@@ -6,13 +6,15 @@ module XapiMiddleware
 
     after_initialize :set_data
 
+    validates :verb_id, presence: true, format: { with: URI::DEFAULT_PARSER.make_regexp, message: "is not a valid URL" }
+
     def set_data
       @actor = XapiMiddleware::Actor.new(actor)
       @object = XapiMiddleware::Object.new(object)
       @result = XapiMiddleware::Result.new(result) if result.present?
       self.object_identifier = @object.id
       self.actor_name = @actor.name
-      self.statement_json = as_json
+      self.statement_json = prepare_json
     end
 
     def output
@@ -22,13 +24,13 @@ module XapiMiddleware
 
     private
 
-      def as_json
+      def prepare_json
         {
           verb_id: verb_id,
-          object: @object&.as_json,
-          actor: @actor&.as_json,
-          result: @result&.as_json
-        }
+          object: @object,
+          actor: @actor,
+          result: @result
+        }.to_json
       end
 
       def pretty_print
@@ -36,7 +38,7 @@ module XapiMiddleware
       end
 
       def log_output
-        Rails.logger.info Rainbow("#{I18n.t("xapi_middleware.xapi_statement")} => #{statement_json}").yellow
+        Rails.logger.info Rainbow("#{I18n.t("xapi_middleware.xapi_statement")} => #{pretty_print}").yellow
       end
   end
 end
