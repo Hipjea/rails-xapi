b3310b06a4db3c16131bdb746a662842aa8c8742
Hipjea <pierre.duverneix@gmail.com>
2023-12-06 17:06:33 +0100

    object definition

--- app/models/xapi_middleware/object_definition.rb
+++ app/models/xapi_middleware/object_definition.rb
@@ -6,16 +6,35 @@ module XapiMiddleware
 
     # Initializes a new ObjectDefinition instance.
     #
-    # @param [String] name The name of the object definition.
+    # @param [Hash] definition The object definition hash.
     def initialize(definition)
-      @name = definition[:name].presence
+      @name = {}
+      @description = {}
+
+      add_values_to_key(definition[:name], @name)
+      add_values_to_key(definition[:description], @description)
+    end
+
+    # Adds values to the corresponding key, to allow multiple languages values.
+    #
+    # @param [Hash] attr The hash attribute that contains the values.
+    # @param [Hash] key The hash attribute to be defined.
+    def add_values_to_key(attr, key)
+      if attr.is_a?(Hash) && attr.any?
+        attr.each do |k, v|
+          key[k.to_sym] = v
+        end
+      end
     end
 
     # Overrides the Hash class method.
     #
     # @return [Hash] The object definition hash.
     def to_hash
-      {name: @name}.compact
+      {
+        name: @name,
+        description: @description
+      }.compact
     end
   end
 end
