c30f3a8120fcee5a45362e6cd002a315cf3ea138
Hipjea <pierre.duverneix@gmail.com>
2024-06-14 16:58:59 +0200

    validations

--- app/services/xapi_middleware/statement_creator.rb
+++ app/services/xapi_middleware/statement_creator.rb
@@ -1,21 +1,38 @@
 # frozen_string_literal: true
 
 class XapiMiddleware::StatementCreator < ApplicationService
-  attr_reader :user, :data
+  attr_reader :data, :user
 
-  def initialize(data)
+  def initialize(data, user = {})
     @data = data
+    @user = user
   end
 
   def call
+    verify_actor_ifi
+
     statement = XapiMiddleware::Statement.new(statement_json: @data.to_json)
-    unless statement.valid?
-      return {error: statement.errors.full_messages.join(", ")}
+    raise XapiMiddleware::Errors::XapiError, statement.errors.full_messages.join(", ") unless statement.valid?
+
+    statement.save
+    {status: 200, statement: statement}
+  end
+
+  private
+
+  # Ensure that an actor inverse functional identifier (IFI) is present.
+  #
+  # @raise [XapiMiddleware::Errors::XapiError] If the actor is invalid
+  def verify_actor_ifi
+    is_valid = XapiMiddleware::Actor.validate_actor(@data[:actor])
+
+    # Attempt to set the mbox from the optional user param
+    if !is_valid && @user[:email].present?
+      data_actor ||= @data[:actor].merge(mbox: "mailto:#{@user.email}")
+      @data[:actor] = data_actor
     end
 
-    statement
-  rescue XapiMiddleware::Errors::XapiError => e
-    Rails.logger.error("Error in StatementService: #{e.message}")
-    {error: "An unexpected error occurred."}
+    actor = XapiMiddleware::Actor.new(@data[:actor])
+    raise XapiMiddleware::Errors::XapiError, I18n.t("xapi_middleware.errors.actor_ifi_must_be_present") unless actor.valid?
   end
 end
