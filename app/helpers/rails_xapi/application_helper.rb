4794e79f5a9e5026863d1ff6f94a5ac720f0de7d
Hipjea <pierre.duverneix@gmail.com>
2024-09-05 16:28:26 +0200

    context spec

--- app/helpers/rails_xapi/application_helper.rb
+++ app/helpers/rails_xapi/application_helper.rb
@@ -6,5 +6,12 @@ module RailsXapi
     def duration_to_minutes(duration)
       sprintf("%.2f", ActiveSupport::Duration.parse(duration)&.in_minutes)
     end
+
+    # Output the value of a JSON row in a specific locale.
+    def json_value_for_locale(json, locale = I18n.locale)
+      hash = JSON.parse(json)
+      result = hash.select { |key, _value| key.include?(locale.to_s) }
+      result.values.first.to_s || hash.first.value.to_s
+    end
   end
 end
