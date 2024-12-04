195060c1d74ea4910af68934ce98253c1cb0f3f8
Hipjea <pierre.duverneix@gmail.com>
2024-12-04 10:00:32 +0100

    add score getter to result

---
+++ app/services/rails_xapi/query_statement.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# This class manages the query interface for actors.
+class RailsXapi::QueryStatement < ApplicationService
+  def self.by_actor_emails_and_object_id(actor_emails = [], object_id)
+    raise ArgumentError, I18n.t("rails_xapi.errors.malformed_email") unless actor_emails.any?
+
+    mailto_emails = actor_emails.map do |email|
+      email.start_with?("mailto:") ? email : "mailto:#{email}"
+    end
+
+    RailsXapi::Statement.includes([:actor, :verb, :object, :result]).where(actor: {mbox: mailto_emails}, object: {id: object_id})
+  end
+end
