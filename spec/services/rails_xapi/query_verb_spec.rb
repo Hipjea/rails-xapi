716cbaeae6804afacc3d068e3fa0e4704f8e69ba
Hipjea <pierre.duverneix@gmail.com>
2025-04-09 17:23:53 +0200

    query_verb_spec

---
+++ spec/services/rails_xapi/query_verb_spec.rb
@@ -0,0 +1,48 @@
+require "rails_helper"
+
+RSpec.describe RailsXapi::QueryVerb, type: :service do
+  include_context "statement"
+
+  before :all do
+    @statement_1 = build_statement(1, 0)
+    @statement_2 = build_statement(2, 1)
+    @statement_3 = build_statement(3, 2)
+  end
+
+  describe "methods" do
+    it "describes self.verb_ids" do
+      @statement_1.call
+      @statement_2.call
+      @statement_3.call
+
+      verb_ids = RailsXapi::Verb::VERBS_LIST.keys.first(3)
+
+      expect(RailsXapi::QueryVerb.verb_ids).to contain_exactly(*verb_ids)
+    end
+
+    it "describes self.verb_displays" do
+      @statement_1.call
+      @statement_2.call
+      @statement_3.call
+
+      # Get the VERBS_LIST display values and convert them into the expected JSON value.s
+      verb_displays = RailsXapi::Verb::VERBS_LIST.values.first(3).map do |display|
+        {"en-US" => display}.to_json
+      end
+
+      expect(RailsXapi::QueryVerb.verb_displays).to contain_exactly(*verb_displays)
+    end
+
+    it "describes self.verbs" do
+      @statement_1.call
+      @statement_2.call
+      @statement_3.call
+
+      verbs = RailsXapi::Verb::VERBS_LIST.first(3).map do |verb, display|
+        [verb, {"en-US" => display}.to_json]
+      end
+
+      expect(RailsXapi::QueryVerb.verbs).to contain_exactly(*verbs)
+    end
+  end
+end
--- spec/shared/statement.rb
+++ spec/shared/statement.rb
@@ -1,5 +1,17 @@
 RSpec.shared_context "statement" do
   before :all do
+    def build_statement(actor_num, verb_index)
+      RailsXapi::StatementCreator.new({
+        actor: {
+          name: "Actor #{actor_num}",
+          mbox_sha1sum: "sha1:d35132bd0bfc15ada6f5229002b5288d94a46f5#{actor_num}",
+          openid: "http://example.com/object/Actor##{actor_num}"
+        },
+        verb: {id: RailsXapi::Verb::VERBS_LIST.keys[verb_index]},
+        object: {id: "/object/#{actor_num}"}
+      })
+    end
+
     @verb = RailsXapi::Verb.new(id: RailsXapi::Verb::VERBS_LIST.keys[0])
 
     @actor = RailsXapi::Actor.new(
