0c967ba78830eb056bdea03d839507623acb08fb
Hipjea <pierre.duverneix@gmail.com>
2025-04-16 14:48:06 +0200

    generate_start_date_end_date

--- app/services/rails_xapi/query.rb
+++ app/services/rails_xapi/query.rb
@@ -21,6 +21,15 @@ class RailsXapi::Query < ApplicationService
 
   private
 
+  # Set the start_date to the first day of the given month and year,
+  # and the end_date to the last day of the givenn month.
+  private_class_method def self.generate_start_date_end_date(year, month)
+    start_date = Date.new(year, month, 1)
+    end_date = start_date.end_of_month
+
+    [start_date, end_date]
+  end
+
   def statement(id)
     RailsXapi::Statement.includes([:actor, :verb, :object, :context, :result]).find(id)
   end
