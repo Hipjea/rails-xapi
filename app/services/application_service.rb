4ba20c82ff44c49e37bbe8a0e677478c9428f0d6
Hipjea <pierre.duverneix@gmail.com>
2025-04-16 15:04:33 +0200

    generate_start_date_end_date

--- app/services/application_service.rb
+++ app/services/application_service.rb
@@ -4,4 +4,13 @@ class ApplicationService
   def self.call(*, &)
     new(*, &).call
   end
+
+  # Set the start_date to the first day of the given month and year,
+  # and the end_date to the last day of the givenn month.
+  def self.generate_start_date_end_date(year, month)
+    start_date = Date.new(year, month, 1)
+    end_date = start_date.end_of_month
+
+    [start_date, end_date]
+  end
 end
